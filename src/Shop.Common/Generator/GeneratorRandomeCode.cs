5a548e58b83527c96a5f9cd202a41d0cf616a12d
Mojtaba <mojtabapaso@gmail.com>
2024-02-11 15:35:09 +0330

    add GeneratorRandomeCode

---
+++ src/Shop.Common/Generator/GeneratorRandomeCode.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Common.Generator;
+
+public static class GeneratorRandomeCode
+{
+	public static string RandomeCode(int count)
+	{
+		Random rand = new Random();
+		string code = "";
+		for (int i = 0; i < count; i++)
+		{
+			code += rand.Next(10);
+		}
+		return code;
+	}
+
+}

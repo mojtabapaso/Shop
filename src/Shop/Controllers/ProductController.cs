032ba3ba3bed343337753121bf2c27b627ea11d7
Mojtaba <mojtabapaso@gmail.com>
2024-02-14 14:53:01 +0330

    add Details and Search action

--- src/Shop/Controllers/ProductController.cs
+++ src/Shop/Controllers/ProductController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.services.Contracts;
 
 namespace Shop.Controllers;
 
 public class ProductController : Controller
 {
-	public ActionResult Details(int id)
+	private readonly IProductServisec productServisec;
+
+	public ProductController(IProductServisec productServisec)
+	{
+		this.productServisec = productServisec;
+	}
+	public IActionResult Details(int id)
 	{
 		return View();
+	}
+	[HttpGet("/Search")]
+	public async Task<IActionResult> Search(string quary)
+	{
+		var products = await productServisec.SearchAsync(quary);
+		return View(products);
 	}
 }

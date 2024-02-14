520588261c012ecedbbdbc4b2cb408d368de0d05
Mojtaba <mojtabapaso@gmail.com>
2024-02-14 15:02:07 +0330

    create and implimated SearchAsync

--- src/Shop.Services/EFServices/ProductServisec.cs
+++ src/Shop.Services/EFServices/ProductServisec.cs
@@ -1,18 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Shop.DataLayer.context;
 using Shop.Entities;
-using Shop.Services.Contracts;
+using Shop.services.Contracts;
 
-namespace Shop.Services.EFServices;
+namespace Shop.services.EFServices;
 
-public class  ProductServisec : GenericServices<Product>, IProductServisec
+public class ProductServisec : GenericServices<Product>, IProductServisec
 {
 	private readonly IUnitOfWork ـuow;
 	private readonly DbSet<Product> _product;
-	public ProductServisec(IUnitOfWork uow):base(uow)
+	public ProductServisec(IUnitOfWork uow) : base(uow)
 	{
 		ـuow = uow;
 		_product = uow.Set<Product>();
+	}
+
+	public async Task<List<Product>?> SearchAsync(string query)
+	{
+		if (string.IsNullOrEmpty(query))
+		{
+			return null;
+		}
+		query = query.ToLowerInvariant();
+
+		var products = await _product.Where(p => p.Title.ToLower().Contains(query))
+		.Where(p => p.Description.ToLower().Contains(query))
+		.Where(p => p.Brand.ToLower().Contains(query)).ToListAsync();
+
+		return products;
 	}
 }
 

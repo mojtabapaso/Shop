a0a585a34030cc71b659d6f334e531226c309ed6
Mojtaba <mojtabapaso@gmail.com>
2024-02-14 14:51:42 +0330

    add Login action

--- src/Shop/Controllers/AccountController.cs
+++ src/Shop/Controllers/AccountController.cs
@@ -1,28 +1,25 @@
 ﻿using System.Text.RegularExpressions;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Common.Generator;
 using Shop.DataLayer.context;
 using Shop.Entities;
-using Shop.Services.Contracts;
+using Shop.services.Contracts;
 using Shop.ViewModels.Account;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Shop.Controllers;
 
 public class AccountController : Controller
 {
-    private readonly CustomeUserManager userManager;
-    private readonly SignInManager<User> signInManager;
-    private readonly IKaveNegarServic kaveNegarServic;
-    private readonly IConfiguration configuration;
-    private readonly IMongoDbAuthenticationServices mongoDbAuthenticationServices;
-    private readonly IUnitOfWork uow;
-    private readonly ILogger<AccountController> logger;
+	private readonly CustomeUserManager userManager;
+	private readonly SignInManager<User> signInManager;
+	private readonly IKaveNegarServic kaveNegarServic;
+	private readonly IConfiguration configuration;
+	private readonly IMongoDbAuthenticationServices mongoDbAuthenticationServices;
+	private readonly IUnitOfWork uow;
+	private readonly ILogger<AccountController> logger;
 
-    public AccountController(
+	public AccountController(
 	CustomeUserManager userManager,
 
 	IKaveNegarServic kaveNegarServic,
@@ -41,117 +38,145 @@ public class AccountController : Controller
 		this.signInManager = signInManager;
 	}
 	[HttpGet]
-    public IActionResult Auth(string? returnUrl)
-    {
-        if (returnUrl != null)
-            TempData["returnUrl"] = returnUrl;
+	public IActionResult Auth(string? returnUrl)
+	{
+		if (returnUrl != null)
+			TempData["returnUrl"] = returnUrl;
 
-        return View();
-    }
-    [HttpGet]
-    public IActionResult Register()
-    {
-        return View();
-    }
+		return View();
+	}
+	[HttpGet]
+	public IActionResult Register()
+	{
+		return View();
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
-    {
-        List<string> errors = new List<string>();
-        if (ModelState.IsValid)
-        {
-            string code = GeneratorRandomeCode.RandomeCode(6);
-            kaveNegarServic.SendCodeToPhoneNumber(code, registerViewModel.PhoneNumber);
-            await mongoDbAuthenticationServices.CreateAuthUserAsync(registerViewModel.PhoneNumber, code);
+	[HttpPost]
+	public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+	{
+		List<string> errors = new List<string>();
+		if (ModelState.IsValid)
+		{
+			string code = GeneratorRandomeCode.RandomeCode(6);
+			kaveNegarServic.SendCodeToPhoneNumber(code, registerViewModel.PhoneNumber);
+			await mongoDbAuthenticationServices.CreateAuthUserAsync(registerViewModel.PhoneNumber, code);
 
-            return RedirectToAction(nameof(RegisterOTPCode), new { phoneNumber = registerViewModel.PhoneNumber });
+			return RedirectToAction(nameof(RegisterOTPCode), new { phoneNumber = registerViewModel.PhoneNumber });
 
-        }
-        return BadRequest(errors);
-    }
-    [HttpGet]
-    public IActionResult RegisterOTPCode(string phoneNumber)
-    {
-        if (phoneNumber == null)
-        {
-            return BadRequest();
-        }
-        ViewData["phoneNumber"] = phoneNumber;
-        return View();
-    }
+		}
+		return BadRequest(errors);
+	}
+	[HttpGet]
+	public IActionResult RegisterOTPCode(string phoneNumber)
+	{
+		if (phoneNumber == null)
+		{
+			return BadRequest();
+		}
+		ViewData["phoneNumber"] = phoneNumber;
+		return View();
+	}
 
 
-    [HttpPost]
-    public async Task<IActionResult> RegisterOTPCode(CodeOTPViewModel codeOTPViewModel)
-    {
-        if (ModelState.IsValid && await mongoDbAuthenticationServices.PhoneNumberIsValidAsync(codeOTPViewModel.PhoneNumber))
-        {
-            var otp = await mongoDbAuthenticationServices.GetUserByPhoneNumberAsync(codeOTPViewModel.PhoneNumber);
-            if (otp != null)
-            {
-                var phoneNumber = otp.GetElement("PhoneNumber").Value.AsString;
-                var code = otp.GetElement("Code").Value.AsString;
-                if (code == codeOTPViewModel.Code)
-                {
-                    var user = new User()
-                    {
-                        UserName = phoneNumber,
-                        PhoneNumber = phoneNumber,
-                        PhoneNumberConfirmed = true,
-                    };
-                    await userManager.CreateAsync(user);
-                    var ss = await uow.SaveChangesAsync();
-                }
-                else if (code != codeOTPViewModel.Code)
-                {
-                    await mongoDbAuthenticationServices.AddTryAsync(codeOTPViewModel.PhoneNumber);
+	[HttpPost, ValidateAntiForgeryToken]
+	public async Task<IActionResult> RegisterOTPCode(CodeOTPViewModel codeOTPViewModel)
+	{
+		if (ModelState.IsValid && await mongoDbAuthenticationServices.PhoneNumberIsValidAsync(codeOTPViewModel.PhoneNumber))
+		{
+			var otp = await mongoDbAuthenticationServices.GetUserByPhoneNumberAsync(codeOTPViewModel.PhoneNumber);
+			if (otp != null)
+			{
+				var phoneNumber = otp.GetElement("PhoneNumber").Value.AsString;
+				var code = otp.GetElement("Code").Value.AsString;
+				if (code == codeOTPViewModel.Code)
+				{
+					var user = new User()
+					{
+						UserName = phoneNumber,
+						PhoneNumber = phoneNumber,
+						PhoneNumberConfirmed = true,
+					};
+					await userManager.CreateAsync(user);
+					var ss = await uow.SaveChangesAsync();
+				}
+				else if (code != codeOTPViewModel.Code)
+				{
+					await mongoDbAuthenticationServices.AddTryAsync(codeOTPViewModel.PhoneNumber);
 					return View();
-                }
-            }
-        }
-        return BadRequest();
-    }
+				}
+			}
+		}
+		return BadRequest();
+	}
 
 
-    [HttpGet]
-    public IActionResult Login()
-    {
-        return View();
-    }
-    [HttpPost]
-    public async Task<IActionResult> Login(LoginViewModel loginViewModel)
-    {
-        if (!ModelState.IsValid)
-            return View(loginViewModel);
-        var user = await userManager.FindByPhoneNumberAsync(loginViewModel.PhoneNumber);
-        if (user == null)
-            ModelState.AddModelError("", "شمراه تلفن شما یافت نشد ");
+	[HttpGet]
+	public IActionResult Login()
+	{
+		return View();
+	}
+	[HttpPost,ValidateAntiForgeryToken]
+
+	public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+	{
+		if (!ModelState.IsValid)
 			return View(loginViewModel);
-        string code = GeneratorRandomeCode.RandomeCode(6);
-        await mongoDbAuthenticationServices.CreateAuthUserAsync(loginViewModel.PhoneNumber ,code);
-		return View(loginViewModel);
-    }
+		var user = await userManager.FindByPhoneNumberAsync(loginViewModel.PhoneNumber);
+		if (user == null)
+		{
+			ModelState.AddModelError("", "شمراه تلفن شما یافت نشد ");
+			return View(loginViewModel);
+		}
 
-    public async Task<IActionResult> Logout()
-    {
-        await signInManager.SignOutAsync();
-        return RedirectToAction("Index", "Home");
-    }
+		string code = GeneratorRandomeCode.RandomeCode(6);
+		await mongoDbAuthenticationServices.CreateAuthUserAsync(loginViewModel.PhoneNumber, code);
 
-    [HttpPost]
-    public JsonResult RegexValidatePhoneNumber(string phoneNumber)
-    {
-        if (Regex.IsMatch(phoneNumber, @"^09\d{9}$"))
-            return Json(true);
-        return Json(false);
-    }
+		return RedirectToAction(nameof(LoginOTP), new { phoneNumber = loginViewModel.PhoneNumber });
+	}
 
-    [HttpPost]
-    public JsonResult CheckUserName(string userName)
-    {
-        var user = userManager.FindByPhoneNumberAsync(userName);
-        if (user != null)
-            return Json(true);
-        return Json(false);
-    }
+	[HttpGet]
+	public IActionResult LoginOTP(string phoneNumber)
+	{
+		if (string.IsNullOrEmpty(phoneNumber))
+		{
+			return BadRequest();
+		}
+		ViewData["phoneNumber"] = phoneNumber;
+		return View();
+	}
+	[HttpPost]
+	public async Task<IActionResult> LoginOTP(CodeOTPViewModel codeOTPViewModel)
+	{
+		if (!ModelState.IsValid)
+			return View(codeOTPViewModel);
+
+		var user = await userManager.FindByPhoneNumberAsync(codeOTPViewModel.PhoneNumber);
+		await signInManager.SignInAsync(user, true, null);
+		return RedirectToAction("Index", "Home");
+
+	}
+
+
+	public async Task<IActionResult> Logout()
+	{
+		await signInManager.SignOutAsync();
+		return RedirectToAction("Index", "Home");
+	}
+
+	[HttpPost]
+	public JsonResult RegexValidatePhoneNumber(string phoneNumber)
+	{
+		if (Regex.IsMatch(phoneNumber, @"^09\d{9}$"))
+			return Json(true);
+		return Json(false);
+	}
+
+	[HttpPost]
+	public JsonResult CheckUserName(string userName)
+	{
+		var user = userManager.FindByPhoneNumberAsync(userName);
+		if (user != null)
+			return Json(true);
+		return Json(false);
+	}
 }

92447031f2a1ce299caf2d9b58343a07baf3324f
Mojtaba <mojtabapaso@gmail.com>
2024-02-14 14:58:24 +0330

    change name field

--- src/Shop.ViewModels/Account/RegisterViewModel.cs
+++ src/Shop.ViewModels/Account/RegisterViewModel.cs
@@ -1,16 +1,18 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Resources;
+using System.Security.AccessControl;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Common.Constants;
 // ^([\w\.\-]+)@([\w\-]+)((\.(\w){2,})+)$
-namespace Shop.ViewModels.Account
+
+namespace Shop.ViewModels.Account;
+
+public class RegisterViewModel
 {
-	public class RegisterViewModel
-    {
-        [DisplayName("شماره تلفن")]
-        [Required(ErrorMessage = AttributesErrorMessages.RequiredMessage)]
-        [StringLength(maximumLength: 11, ErrorMessage = AttributesErrorMessages.StringLenghtMessage, MinimumLength = 11)]
-        [Remote("RegexValidatePhoneNumber", "Account", ErrorMessage = AttributesErrorMessages.RemoteMessageReGexPhoneNumber, HttpMethod = "POST")]
-        public string? PhoneNumber { get; set; }
-    }
+	[DisplayName("Phone Number")]
+	[Required(ErrorMessage = AttributesErrorMessages.RequiredMessage)]
+	[StringLength(maximumLength: 11, ErrorMessage = AttributesErrorMessages.StringLenghtMessage, MinimumLength = 11)]
+	[Remote("RegexValidatePhoneNumber", "Account", ErrorMessage = AttributesErrorMessages.RemoteMessageReGexPhoneNumber, HttpMethod = "POST")]
+	public string? PhoneNumber { get; set; }
 }

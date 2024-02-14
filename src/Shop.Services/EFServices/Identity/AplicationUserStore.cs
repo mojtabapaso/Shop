06eff0942759b048453fbe66ac165556b3e20bf7
Mojtaba <mojtabapaso@gmail.com>
2024-02-14 15:00:48 +0330

    change identity to use default

--- src/Shop.Services/EFServices/Identity/AplicationUserStore.cs
+++ src/Shop.Services/EFServices/Identity/AplicationUserStore.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Shop.DataLayer.context;
 using Shop.Entities;
 
-namespace Shop.Services.EFServices.Identity;
+namespace Shop.services.EFServices.Identity;
 
-public class AplicationUserStore:UserStore<User, Role, ShopDbContext,int, UserClaim, UserRole, UserLogin, UserToken, RoleClaim>
+public class AplicationUserStore:UserStore<User, IdentityRole<string>, ShopDbContext,string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, IdentityUserToken<string>, IdentityRoleClaim<string>>
 {
 	public AplicationUserStore(IUnitOfWork uow ,IdentityErrorDescriber identityErrorDescriber=null):base((ShopDbContext)uow, identityErrorDescriber)
 	{

a69aed860fb25bbfe0e03767b1611502b26ea757
Mojtaba <mojtabapaso@gmail.com>
2024-02-19 14:32:48 +0330

    Add AccountController of Admin Area

---
+++ src/Shop/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Entities;
+
+namespace Shop.Areas.Admin.Controllers;
+
+[Area(AreaConstants.AdminArea)]
+public class AccountController : Controller
+{
+	private readonly SignInManager<User> signInManager;
+
+	public AccountController(SignInManager<User> signInManager)
+	{
+		this.signInManager = signInManager;
+	}
+	public IActionResult Index()
+	{
+		return View();
+	}
+	public async Task<IActionResult> Logout()
+	{
+		await signInManager.SignOutAsync();
+		return RedirectToRoute("/");
+	}
+
+}

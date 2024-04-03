e063328cdc6547279364ced312c1709a356a17a4
mojtaba <mojtabapaso@gmail.com>
2024-04-03 16:28:14 +0330

    Add Common folder

---
+++ src/Shop.Common/Helper/CustomeIdentityError.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shop.Helper;
+
+public class CustomeIdentityError:IdentityErrorDescriber
+{
+    public override IdentityError ConcurrencyFailure()
+    {
+        return new IdentityError
+        {
+            Code = nameof(ConcurrencyFailure),
+            Description = "خطای ناشناخته"
+        };
+    }
+    public override IdentityError DuplicateEmail(string email)
+    {
+        return new IdentityError {
+        Code = nameof(DuplicateEmail),
+        Description = $"ایمیل تکراری است : {email}"
+        };
+    }
+
+}
---
+++ src/Shop.Common/Helper/MyPasswordVa;idator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shop.Entities;
+
+namespace Shop.Common.Helper;
+
+public class MyPasswordValidator : IPasswordValidator<User>
+{
+    List<string> CommonPassword = new List<string>()
+    {
+        "123456","654321",
+    };
+    public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string? password)
+    {
+        if (CommonPassword.Contains(password))
+        {
+            var result =  IdentityResult.Failed(new IdentityError { 
+            Code = "CommonPassword",
+            Description = "Password is very simple",
+            });
+            return Task.FromResult(result);
+        }
+        return Task.FromResult(IdentityResult.Success);
+        throw new NotImplementedException();
+    }
+}
